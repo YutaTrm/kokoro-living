7df64da798dfece806a523ad83e3ce0ee6b57df1
YutaTRM <murakumo0079@gmail.com>
2025-11-29 21:31:34 +0900

    いいねボタン押した時のブックマークずれを修正

--- components/PostActionButtons.tsx
+++ components/PostActionButtons.tsx
@@ -37,7 +37,7 @@ export default function PostActionButtons({
     <HStack space="4xl" className="items-center">
       {/* 返信ボタン */}
       {showReplyButton && (
-        <Pressable onPress={onReply} className="flex-row items-center">
+        <Pressable onPress={onReply} className="flex-row items-center mr-[1rem]">
           <HStack space="xs" className="items-center">
             <Icon as={MessageCircle} size={iconSize} className="text-typography-500" />
             {repliesCount > 0 && (
@@ -56,13 +56,13 @@ export default function PostActionButtons({
             className={isLiked ? 'text-secondary-500 fill-secondary-500' : 'text-typography-500 fill-none'}
           />
         </Pressable>
-        {likesCount > 0 && (
-          <Pressable onPress={onLikesCountPress}>
+        <Pressable onPress={onLikesCountPress} className="min-w-[2rem]">
+          {likesCount > 0 && (
             <Text className={`${textClass} ${isLiked ? 'text-secondary-500' : 'text-typography-500'} pl-1`}>
               {likesCount}
             </Text>
-          </Pressable>
-        )}
+          )}
+        </Pressable>
       </HStack>
 
       {/* ブックマークボタン */}
